3dfef380a4012673b583f2bfd71b0926a4fcf2e0
ramirojacob7289-lang <ramirojacob7289@gmail.com>
2026-03-31 00:19:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 <html lang="zxx">
 
 <head>
+    <!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,5017480,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5017480&101" alt="web hit counter" border="0"></a></noscript>
+<!-- Histats.com  END  -->
     <meta charset="UTF-8">
     <meta name="description" content="Ashion Template">
     <meta name="keywords" content="Ashion, unica, creative, html">
